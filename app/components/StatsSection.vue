c6e08956f63e939771c058e5d2fde03dabecaee7
Oleg <ikslprint@gmail.com>
2026-08-11 12:56:49 +0300

    rework stats section

--- app/components/StatsSection.vue
+++ app/components/StatsSection.vue
@@ -7,16 +7,16 @@
           <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Років досвіду</div>
         </div>
         <div>
-          <div class="font-noto text-4xl md:text-5xl font-black text-white mb-2">40+</div>
-          <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Успішних проектів</div>
+          <div class="font-noto text-4xl md:text-5xl font-black text-white mb-2">60+</div>
+          <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Реалізованих проєктів</div>
         </div>
         <div>
           <div class="font-noto text-4xl md:text-5xl font-black text-white mb-2">100%</div>
           <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Кастомний код</div>
         </div>
         <div>
-          <div class="font-noto text-4xl md:text-5xl font-black text-white mb-2">24/7</div>
-          <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Підтримка</div>
+          <div class="font-noto text-4xl md:text-5xl font-black text-white mb-2">14 днів</div>
+          <div class="text-sm text-gray-400 uppercase tracking-widest font-bold">Безкоштовна підтримка</div>
         </div>
       </div>
     </UContainer>
