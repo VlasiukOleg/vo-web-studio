07ad14953849794695353ce0448c539e37db9d6c
Oleg <ikslprint@gmail.com>
2026-08-20 19:17:43 +0300

    add og-image

--- app/app.vue
+++ app/app.vue
@@ -8,3 +8,17 @@
     </div>
   </UApp>
 </template>
+
+
+<script setup>
+useSeoMeta({
+  title: 'РОЗРОБЛЕНО - Студія цифрових рішень',
+  description: 'Комплексні рішення для розвитку вашого бізнесу в інтернеті. Створення сайтів від прототипу до успішного релізу.',
+  ogTitle: 'РОЗРОБЛЕНО - Студія цифрових рішень',
+  ogDescription: 'Комплексні рішення для розвитку вашого бізнесу в інтернеті. Створення сайтів від прототипу до успішного релізу.',
+  ogImage: 'https://vo-web-studio.vercel.app/og-image.png',
+  ogSiteName: 'РОЗРОБЛЕНО',
+  ogUrl: 'https://vo-web-studio.vercel.app/',
+  twitterCard: 'summary_large_image',
+})
+</script>
